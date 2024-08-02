9c8d28bdc81c6700360dd2dbda7fb70cc675ede4
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-08-02 17:06:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.educandoweb.workshopmongo.resources;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+
+}
---
+++ src/main/java/com/educandoweb/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.educandoweb.workshopmongo.service;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.repository.PostRepository;
+import com.educandoweb.workshopmongo.service.exception.ObjectNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+}
